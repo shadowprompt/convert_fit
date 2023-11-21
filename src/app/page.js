93c5d99211e9d31d75965455380f493790994fc8
hxiong <hxiong@ctrip.com>
2023-11-21 19:26:10 +0800

    [fix]微调

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 'use client'
 
 import React, { useState, useEffect, useRef } from 'react';
-import { UploadOutlined } from '@ant-design/icons';
-import { Row, Col, Divider, Radio, List } from 'antd';
+import { Row, Col, Divider } from 'antd';
 import Nav from '@/components/Nav';
 import Bottom from '@/components/Bottom';
 import * as echarts from 'echarts';
@@ -171,14 +170,14 @@ export default function() {
           每一次运动锻炼了我们的身体，值得留存在我们的记录里，不负每一滴汗水，尊重每一次付出。
         </p>
         <p>
-          本工具旨在为各位跑友转换运动记录数据，支持将<b>华为运动健康</b>、<b>Zepp Life（原小米运动）</b>官方导出的运动数据转换成业内通用的fit（推荐）或tcx格式，然后即可顺利导入主流的运动平台，比如高驰、佳明、RQrun、Strava等。后续可能会支持fit和tcx格式互转、支持其它运动平台等功能。
+          本工具旨在为各位跑友转换运动记录数据，支持将<b>华为运动健康</b>、<b>Zepp Life（原小米运动）</b>官方导出的运动数据转换成业内通用的fit（推荐）或tcx格式，然后即可顺利导入主流的运动平台，比如<b>高驰</b>、<b>佳明</b>、<b>RQrun</b>、<b>Strava</b>等。后续可能会支持fit和tcx格式互转、支持其它运动平台等功能。
         </p>
         <Divider>转换请求响应统计图</Divider>
         <div id="chart" style={{height: '420px'}}></div>
         <Divider>转换效果展示</Divider>
         <Row gutter={16}>
           <Col className="gutter-row" span={10}>
-            <Divider>华为运动记录转换后导入高驰</Divider>
+            <Divider>华为导入高驰</Divider>
             <p>
               官方导出的数据细节很齐全，转换后效果还是不错的
             </p>
@@ -190,7 +189,7 @@ export default function() {
             </div>
           </Col>
           <Col className="gutter-row" span={10} offset={4}>
-            <Divider>小米运动记录转换后导入高驰</Divider>
+            <Divider>小米导入高驰</Divider>
             <p>
               可惜的是官方导出的数据极其不全（无GPS，只有零星的几个时间点有步频信息），转换后基本上只能看看心率了。
             </p>

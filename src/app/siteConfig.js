d26baeda8bce7a0b3f012aab905761e9c0b8d0a1
shadowprompt <huixiong.cn@gmail.com>
2023-12-09 21:01:36 +0800

    [fix]改用@daozhao/config

--- src/app/siteConfig.js
+++ src/app/siteConfig.js
@@ -1,5 +1,6 @@
+import { DAOZHAO_GATEWAY_SERVER } from '@daozhao/config';
 export default {
   siteUrl: 'https://convert.fit',
   siteName: '运动记录转换工具',
-  apiBase: 'https://gateway.daozhao.com.cn',
+  apiBase: DAOZHAO_GATEWAY_SERVER,
 }

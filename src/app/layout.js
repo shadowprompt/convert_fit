1993f8c5ddabce2262670cd676ea4f28f9263caa
shadowprompt <huixiong.cn@gmail.com>
2023-12-30 20:38:27 +0800

    [fix]加入微软clarity脚本

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,6 +47,14 @@ export default function RootLayout({ children }) {
         s.parentNode.insertBefore(el, s);
         })(window);`
       }}></Script>
+      <Script dangerouslySetInnerHTML={{
+        __html: `
+        (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "kdxc812n25");`
+      }}></Script>
     </html>
   )
 }

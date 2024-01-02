848eff3e6b0dc6c59a12a1ae67bb18a9c08a55a5
hxiong <hxiong@ctrip.com>
2024-01-02 10:59:36 +0800

    [fix]调整样式

--- src/components/server/ServerBottom.js
+++ src/components/server/ServerBottom.js
@@ -9,8 +9,8 @@ async function ServerBottom() {
   const result = await getRecord();
 
   return (
-    <div className="countWrapper" style={{textAlign: 'center'}}>
-      <p>累计响应转换请求 <span style={{fontWeight: 'bold', color: '#ff0000', fontSize: '24px'}}>{result.count} </span> 次，转换运动记录 <span style={{fontWeight: 'bold', color: '#ff0000', fontSize: '24px'}}>{result.sportsCount} </span>条</p>
+    <div className="countWrapper">
+      <p>累计响应转换请求 <span className="highlight">{result.count} </span> 次，转换运动记录 <span className="highlight">{result.sportsCount} </span>条</p>
       <p><span style={{display: 'none'}}>{result.successCount}</span></p>
       <p><a href={siteConfig.siteUrl + '/sitemap.xml'} target="_blank">{siteConfig.siteName}</a></p>
       <p><a href="https://beian.miit.gov.cn/" target="_blank" title={siteConfig.icpNumber}
